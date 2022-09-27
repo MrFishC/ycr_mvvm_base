5f1dee030fe16e235b51b14db684bd9b50397226
jack <3453160992@qq.com>
2022-09-27 11:24:42 +0800

    init

---
+++ lib_wrapper_mvvm/src/main/java/it/jack/lib_wrapper_mvvm/base/view/BaseWrapperActivity.kt
@@ -0,0 +1,40 @@
+package it.jack.lib_wrapper_mvvm.base.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.databinding.ViewDataBinding
+
+/**
+ * @创建者 Jack
+ * @创建时间 2022/8/26 0026 11:03
+ * @描述
+ */
+abstract class BaseWrapperActivity<VB : ViewDataBinding>(open var block: (LayoutInflater) -> VB) :
+    AppCompatActivity(), IBaseView {
+
+    protected val mBinding: VB by lazy { block(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        prepareParam()
+        super.onCreate(savedInstanceState)
+        setContentView(mBinding.root)
+        perpareWork()
+    }
+
+    protected open fun perpareWork() {
+        prepareData()
+        prepareListener()
+    }
+
+    override fun prepareData() {}
+
+    override fun prepareListener() {}
+
+    open fun hideDialog(){
+
+    }
+
+    open fun visibleDialog() {
+
+    }
+}
